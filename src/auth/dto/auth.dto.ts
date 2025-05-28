7428659bfc6ea633565e2df459ee46d61f38ff8d
Asad Saeed <asadsaeed.dev@gmail.com>
2025-05-28 15:43:26 +0500

    feat:Swagger UI Implemented

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -6,19 +6,36 @@ import {
   MinLength,
 } from 'class-validator';
 import { UserRole } from '../../common/enum/user/roles.enum';
+import { ApiProperty } from '@nestjs/swagger';
 
 // Register DTO
 export class RegisterDto {
+  @ApiProperty({
+    description: 'The name of the user',
+    example: 'John Doe',
+  })
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({
+    description: 'The email of the user',
+    example: 'john.doe@example.com',
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description: 'The password of the user',
+    example: 'password123',
+  })
   @IsNotEmpty()
   @MinLength(6)
   password: string;
 
+  @ApiProperty({
+    description: 'The role of the user',
+    example: UserRole.USER,
+  })
   @IsNotEmpty()
   @IsString()
   @IsIn(Object.values(UserRole))
@@ -27,24 +44,44 @@ export class RegisterDto {
 
 // Login DTO
 export class LoginDto {
+  @ApiProperty({
+    description: 'The email of the user',
+    example: 'john.doe@example.com',
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description: 'The password of the user',
+    example: 'password123',
+  })
   @IsNotEmpty()
   password: string;
 }
 
 // Forgot Password DTO
 export class ForgotPasswordDto {
+  @ApiProperty({
+    description: 'The email of the user',
+    example: 'john.doe@example.com',
+  })
   @IsEmail()
   email: string;
 }
 
 // Reset Password DTO
 export class ResetPasswordDto {
+  @ApiProperty({
+    description: 'The token of the user',
+    example: '1234567890',
+  })
   @IsNotEmpty()
   token: string;
 
+  @ApiProperty({
+    description: 'The password of the user',
+    example: 'password123',
+  })
   @IsNotEmpty()
   @MinLength(6)
   password: string;
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Splitify APIs')
+    .setDescription('APIs for managing expenses')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   console.log('Server is running on port:', process.env.PORT!);
   await app.listen(process.env.PORT!);
 }
